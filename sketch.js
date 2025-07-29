357c821fa149ff1a350c504a9fb5a9785bd744b1
sofhia <sofhia.lima@escola.pr.gov.br>
2025-07-29 11:53:39 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,7 +4,7 @@ function setup() {
 }
 
 function draw() {
-  background(#2E8B57); //cor de fundo
+  background(#F0E68C); //cor de fundo
   rotateY(millis()/1000); // rotação da terra
   texture(terra); //textura da terra, imagem.
   sphere(200, 160); //esfera 3D
